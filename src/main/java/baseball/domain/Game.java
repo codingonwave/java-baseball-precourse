2701a95187ba328065071aec509b033e5f895428
codingonwave <codingonwave@gmail.com>
2021-10-02 22:36:59 +0900

    feat: Game 객체에 스트라이크 개수를 세는 countStrike 메소드 추가

--- src/main/java/baseball/domain/Game.java
+++ src/main/java/baseball/domain/Game.java
@@ -1,5 +1,8 @@
 package baseball.domain;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Game {
 
     private final GameRule rule;
@@ -25,4 +28,37 @@ public class Game {
         answer = generator.generate();
     }
 
+    public int countStrike(String input) throws IllegalAccessException{
+        if (!playing) throw new IllegalAccessException("아직 게임이 시작되지 않았습니다");
+
+        List<String> inputNumbers = splitToNumberList(input);
+        List<String> answers = splitToNumberList(answer);
+
+        int strike = 0;
+
+        for (int index = 0; index < inputNumbers.size(); index++) {
+            strike += isStrike(inputNumbers.get(index), answers.get(index));
+        }
+
+        return strike;
+    }
+
+    public String getAnswer() {
+        return answer;
+    }
+
+    private List<String> splitToNumberList(String input) {
+        String[] split = input.split("");
+        return Arrays.asList(split);
+    }
+
+    private int isStrike(String input, String answer) {
+        if (input.equals(answer)) return 1;
+
+        return 0;
+    }
+
+    public void setAnswer(String answer) {
+        this.answer = answer;
+    }
 }
--- src/test/java/baseball/domain/GameTest.java
+++ src/test/java/baseball/domain/GameTest.java
@@ -1,13 +1,15 @@
 package baseball.domain;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Nested;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.*;
 
 @DisplayName("Game 클래스")
 class GameTest {
+    Game game;
 
     @Nested
     @DisplayName("start 메소드는")
@@ -16,10 +18,49 @@ class GameTest {
         @Nested
         @DisplayName("호출되면")
         class Context_call {
+            @BeforeEach
+            void callStartMethod() {
+                game = new Game(new GameRule(3, 1, 9));
+                game.start();
+            }
+
             @DisplayName("Game 객체의 playing 속성에 true를 할당한다")
             @Test
             void it_set_playing_true() {
+                assertThat(game.isPlaying()).isTrue();
+            }
 
+            @DisplayName("Game 객체의 answer 속성에 새롭게 생성된 정답을 할당한다")
+            @Test
+            void it_set_answer() {
+                assertThat(game.getAnswer()).isNotEmpty();
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("countStrike 메소드는")
+    class Describe_countStrike {
+
+        GameRule rule = new GameRule(3, 1, 9);
+        Game game = new Game(rule);
+
+        @Nested
+        @DisplayName("사용자가 입력한 3자리 숫자를 받아서")
+        class Context_input {
+
+            @BeforeEach
+            void setAnswer() {
+                game.start();
+                game.setAnswer("123");
+            }
+
+            @DisplayName("정답과 비교하여 스트라이크 개수를 리턴한다")
+            @ParameterizedTest(name = "input: {0}, strike:{1}")
+            @CsvSource({"123, 3", "125, 2","321, 1", "456, 0"})
+            void it_count_strike(String input, Integer expected) throws IllegalAccessException {
+                int actual = game.countStrike(input);
+                assertThat(actual).isEqualTo(expected);
             }
         }
     }
